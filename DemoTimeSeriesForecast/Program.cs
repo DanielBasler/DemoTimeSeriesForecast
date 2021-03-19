4d87dd213b7dba662fa45bd302bc0b3776cb0745
Daniel Basler <daniel.basler@web.de>
2021-03-19 09:36:44 +0100

    Add project files.

---
+++ DemoTimeSeriesForecast/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.ML.Data;
+using Microsoft.ML;
+using Microsoft.ML.Transforms.TimeSeries;
+
+namespace DemoTimeSeriesForecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MLContext();
+
+            var data = context.Data.LoadFromTextFile<ModelInput>($"C:/temp/Zeitdaten.csv",
+                hasHeader: true, separatorChar: ';');
+
+            var pipeline = context.Forecasting.ForecastBySsa(
+                nameof(ModelOutput.ForecastedEnergy),
+                nameof(ModelInput.EnergyDemand),
+                windowSize: 7,
+                seriesLength: 30,
+                trainSize: 365,
+                horizon: 4);
+
+            var model = pipeline.Fit(data);
+
+            var forecastingEngine = model.CreateTimeSeriesEngine<ModelInput, ModelOutput>(context);
+
+            var forecasts = forecastingEngine.Predict();
+
+            Console.WriteLine("Energie-Prognose");
+            Console.WriteLine("----------------");
+            foreach (var forecast in forecasts.ForecastedEnergy)
+            {
+                Console.WriteLine(forecast);
+            }
+        }
+    }
+
+    public class ModelInput
+    {
+        [LoadColumn(0)]
+        public DateTime Date { get; set; }
+
+        [LoadColumn(1)]
+        public float EnergyDemand { get; set; }
+    }
+
+    public class ModelOutput
+    {
+        public float[] ForecastedEnergy { get; set; }
+    }
+
+}
